4a65cf4b04fa8de00fd876b2c379d73f47d67736
davidweichiang <dchiang@nd.edu>
2018-05-24 06:16:25 -0400

    use PyPDF2 to merge pages instead of pdfpages; this preserves hyperrefs

--- pdfstamp.py
+++ pdfstamp.py
@@ -9,11 +9,14 @@ import PyPDF2 as pdf
 import decimal
 
 template = string.Template(r"""\documentclass{article}
-\usepackage[paperwidth=${w}pt,paperheight=${h}pt]{geometry}
-\usepackage{pdfpages}
+\usepackage[paperwidth=${w}pt,paperheight=${h}pt,margin=0pt]{geometry}
 \font\stampfont=phvb at 8pt
+\setlength{\unitlength}{1pt}
 \begin{document}
-\includepdf[pages=-,picturecommand={\put($x,$y){\makebox[0pt][c]{\stampfont $stamp}}}]{$inpdf}%
+\noindent
+\begin{picture}(${w},${h})
+\put($x,$y){\makebox[0pt][c]{\stampfont $stamp}}%
+\end{picture}
 \end{document}
 """)
 
@@ -28,15 +31,25 @@ def papersize(inpdf):
 
 def pdfstamp(inpdf, outpdf, stamp, pdflatex):
     w, h = papersize(inpdf)
-    y = h - decimal.Decimal(42)
+    y = h - decimal.Decimal(42) # 42pt from top
     x = w / decimal.Decimal(2)
         
     with tempfile.TemporaryDirectory() as tmpdir:
         with open(os.path.join(tmpdir, "stamp.tex"), "w") as f:
-            shutil.copy(inpdf, os.path.join(tmpdir, "paper.pdf"))
-            f.write(template.substitute(stamp=stamp, inpdf="paper.pdf", w=w, h=h, x=x, y=y))
+            f.write(template.substitute(stamp=stamp, w=w, h=h, x=x, y=y))
         subprocess.run([pdflatex, "stamp.tex"], cwd=tmpdir)
-        shutil.move(os.path.join(tmpdir, "stamp.pdf"), outpdf)
+
+        stampreader = pdf.PdfFileReader(open(os.path.join(tmpdir, "stamp.pdf"), "rb"))
+        reader = pdf.PdfFileReader(open(inpdf, "rb"))
+        writer = pdf.PdfFileWriter()
+        #for i in range(reader.numPages):
+        for i in [0]:
+            page = stampreader.getPage(0)
+            content = reader.getPage(i)
+            page.mergePage(content)
+            writer.addPage(page)
+        with open(outpdf, "wb") as outfile:
+            writer.write(outfile)
 
 if __name__ == "__main__":
     import sys
